0b8bdcb33e4dec6a8ab6c8624af6b2c4d6194022
ygg404 <2279834998@qq.com>
2020-07-04 17:48:25 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import index from '@/components/Index'
 import comic from "@/components/comic";
 import section from '@/components/section'
+import gdetail from "@/components/h5game/gdetail";
+import h5index from "@/components/h5game/index"
 
 Vue.use(Router)
 
@@ -22,6 +24,16 @@ export default new Router({
       path: '/section',
       name: 'section',
       component: section
+    },
+    {
+      path: '/gdetail',
+      name: 'gdetail',
+      component: gdetail
+    },
+    {
+      path: '/h5index',
+      name: 'h5index',
+      component: h5index
     }
     // {
     //   path: '/hello',
